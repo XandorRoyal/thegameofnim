9f8500a47ac3ec2618d9a59b715cf049c4fe2669
XandorRoyal <58148558+XandorRoyal@users.noreply.github.com>
2024-01-24 18:01:21 -0700

    Added comments. Added pushing to main when no saved data is found.

--- Controller/loadNamesAndBackground.js
+++ Controller/loadNamesAndBackground.js
@@ -1,25 +1,30 @@
 function retrieveUserNames() {
     console.log('Loading Username');
-    let player1 = localStorage.getItem('player1');
-    let player2 = localStorage.getItem('player2');
+    let player1 = localStorage.getItem('player1'); // Get Player 1 Name
+    let player2 = localStorage.getItem('player2'); // Get Player 2 Name
   
-    if (player1==null) console.log("Loaded Player 1 NULL")
-    else if (player2==null) console.log("Loaded Player 2 Null");
-    else {
-      document.getElementById("player1").innerHTML = player1;
-      document.getElementById('player2').innerHTML = player2;
+    if (player1==null) {
+      console.log("Loaded Player 1 NULL") // If Null, report.
+      location.href='../index.html' // Player 1 is Null, push towards index.
     }
+    else if (player2==null) {
+      console.log("Loaded Player 2 Null"); // ^
+      location.href='../index.html'
+    }
+    // Players would be pushed back to index if they have no saved name.
+    document.getElementById("player1").innerHTML = player1; // Not Null, State Username
+    document.getElementById('player2').innerHTML = player2; // Not Null, State Username
   }
 
-function getBackground() {
+function getBackground() { 
     console.log("Getting Background Colors");
 
-    let background = localStorage.getItem("background");
+    let background = localStorage.getItem("background"); // Loads the saved background color.
 
     document.body.style.backgroundColor = background;
 }
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", (event) => { // When loaded, retrieve saved data and act upon.
     retrieveUserNames();
     getBackground();
 });
