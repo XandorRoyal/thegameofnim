8c4becc2f4b60558d1d8c643aacf8efb41ac7644
XandorRoyal <58148558+XandorRoyal@users.noreply.github.com>
2024-01-24 17:27:45 -0700

    Changed settings to allow cross talk between pages.

--- Controller/mainmenu.js
+++ Controller/mainmenu.js
@@ -1,11 +1,33 @@
-function saveUsernames() {
-  var player1 = document.getElementById('player1').value;
-  var player2 = document.getElementById('player2').value;
+function retrieveUserNames() {
+  console.log('Loading Username');
+  let player1 = localStorage.getItem('player1');
+  let player2 = localStorage.getItem('player2');
 
-  localStorage.setItem('player1', player1);
-  localStorage.setItem('player2', player2);
+  if (player1==null) console.log("Loaded Player 1 NULL")
+  else if (player2==null) console.log("Loaded Player 2 Null");
+  else {
+    document.getElementById("player1").innerHTML = player1;
+    document.getElementById('player2').innerHTML = player2;
+  }
 }
+
+function getBackground() {
+  console.log("Getting Background Colors");
+
+  let background = localStorage.getItem("background");
+
+  document.body.style.backgroundColor = background;
+}
+
+window.addEventListener("load", (event) => {
+  retrieveUserNames();
+  getBackground();
+});
+
 function changeBackground() {
   var newColor = prompt("Enter a color (e.g. 'red', '#00FF00', 'rgb(0, 255, 0)'):");
+
+  localStorage.setItem("background", newColor);
+
   document.body.style.backgroundColor = newColor;
 }
